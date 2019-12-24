08741bd6a47bdea9996b08c8bc4d9dcda7318cb9
Sebastian <sebas.tsj.98@gmail.com>
2019-12-24 21:24:53 +0800

    Refactor NewTask to be a stateful component.
    Fix issue where form remains in focus even after submitting.
    
    Fix tags not clearing after submitting.
    
    Prevent using "Enter" to submit form

--- app/javascript/components/NewTask.js
+++ app/javascript/components/NewTask.js
@@ -1,64 +1,110 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import Chip from '@material-ui/core/Chip';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
-const useStyles = makeStyles(theme => ({
+
+const styles = {
     root: {
-      '& > *': {
-        margin: theme.spacing(1),
-      },
+        '& > *': {
+          marginTop: 8,
+          marginBottom: 8,
+        },
     },
-}));
-
-const NewTask = (props) => {
-    const classes = useStyles();
-    let formFields = {}
-
-    return (
-        <form 
-            className={classes.root}
-            onSubmit={(event) => {
-                    event.preventDefault();
-                    props.handleFormSubmit(formFields.title.value, 
-                    formFields.description.value, formFields.tag_list.value); 
-                    event.target.reset();
-                }
-            }
-        >
-
-            <TextField 
-                id="outlined-full-width" 
-                variant="outlined"
-                fullWidth 
-                label="Title"
-                margin="normal"
-                inputRef={input => formFields.title = input} 
-            />
-            
-            <TextField 
-                id="outlined-full-width" 
-                variant="outlined"
-                fullWidth 
-                multiline
-                label="Description"
-                margin="normal"
-                inputRef={input => formFields.description = input} 
-            />
-
-            <TextField 
-                id="standard-basic" 
-                placeholder="Task Tags" 
-                inputRef={input => formFields.tag_list = input}
-            />       
-
-            <Fab size="small" color="primary" aria-label="add" type="submit">
-                <AddIcon />            
-            </Fab>
-
-        </form>
-      )
 };
 
-export default NewTask;
+
+class NewTask extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            description: '',
+            tag_list: [],
+        }
+    }
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <form 
+                className={classes.root}
+                autoComplete="off"
+                onSubmit={(event) => {
+                        event.preventDefault();
+                        event.persist();
+                        this.props.handleFormSubmit(this.state.title, 
+                            this.state.description, this.state.tag_list);   
+                        this.setState({title: '', description: '', tag_list: []})     
+                }}
+            >
+
+                <TextField 
+                    id="outlined-full-width" 
+                    variant="outlined"
+                    fullWidth 
+                    label="Title"
+                    margin="normal"
+                    value={this.state.title}
+                    onKeyPress={event => {
+                        if (event.key === 'Enter') event.preventDefault();
+                    }}
+                    onChange={(event)=>{this.setState({title: event.target.value})}}
+                />
+                
+                <TextField 
+                    id="outlined-full-width" 
+                    variant="outlined"
+                    fullWidth 
+                    multiline
+                    label="Description"
+                    margin="normal"
+                    value={this.state.description}
+                    onKeyPress={event => {
+                        if (event.key === 'Enter') event.preventDefault();
+                    }}
+                    onChange={(event)=>{this.setState({description: event.target.value})}}
+                />
+
+                <Autocomplete
+                    multiple
+                    freeSolo
+                    id="tags-filled"
+                    options={this.props.allTags.map(tag => tag.name)}
+                    onKeyPress={event => {
+                        if (event.key === 'Enter') event.preventDefault();
+                    }}
+                    value={this.state.tag_list}
+                    onChange={(event, value) => this.setState({tag_list: value})}
+                    renderTags={(value, getTagProps) =>
+                        value.map((tag, index) => (
+                            <Chip 
+                                label={tag}
+                                key={index}
+                                className={classes.tag}
+                                {...getTagProps({index})} 
+                            />
+                        ))
+                    }
+                    renderInput={params => <TextField
+                                                {...params}
+                                                variant="outlined"
+                                                label="Add tags"                        
+                                                fullWidth                                            
+                                            />
+                    }
+                />   
+
+                <Fab size="small" color="primary" aria-label="add" type="submit">
+                    <AddIcon />            
+                </Fab>
+
+            </form>
+        )
+    }
+}
+export default withStyles(styles)(NewTask);
