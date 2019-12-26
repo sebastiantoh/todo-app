f409519d45a8e6ddd5673a118e1e9da10ce351f3
Sebastian <sebas.tsj.98@gmail.com>
2019-12-26 16:55:45 +0800

    Refactor TaskForm into TagsInputField

--- app/javascript/components/TaskForm.js
+++ app/javascript/components/TaskForm.js
@@ -1,15 +1,7 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import Chip from '@material-ui/core/Chip';
-import Autocomplete from '@material-ui/lab/Autocomplete';
 
-
-const useStyles = makeStyles(theme => ({
-    tag: {
-        margin: 3,
-    },
-}));
+import TagsInputField from "../components/TagsInputField";
 
 const validateForm = (title, description) => {
     const errors = {}
@@ -28,8 +20,6 @@ const validateForm = (title, description) => {
 }
 
 const TaskForm = (props) => {
-    const classes = useStyles();
-
     let title;
     let description;
     let tags;
@@ -71,36 +61,10 @@ const TaskForm = (props) => {
                     helperText={props.errors.description || ""}
                 />
 
-    tags = <Autocomplete
-                multiple
-                freeSolo
-                id="tags-filled"
-                options={props.allTags.map(tag => tag.name)}
-                onKeyPress={event => {
-                    if (event.key === 'Enter') event.preventDefault();
-                }}
-                // convert tags to lowercase, remove duplicates, and map back to array
-                value={[...new Set(props.tag_list.map(tag => tag.toLowerCase()))]}
-                onChange={props.handleTagUpdate}
-                renderTags={(value, getTagProps) =>
-                    value.map((tag, index) => (
-                        <Chip 
-                            label={tag}
-                            key={index}
-                            className={classes.tag}
-                            {...getTagProps({index})} 
-                        />
-                    ))
-                }
-                renderInput={params => (
-                    <TextField
-                        {...params}
-                        variant="outlined"
-                        fullWidth
-                        label="Add tags"
-                        margin="normal"
-                    />
-                )}
+    tags = <TagsInputField 
+                    allTags={props.allTags}
+                    tag_list={props.tag_list}
+                    handleTagUpdate={props.handleTagUpdate}
             />
     
     return (
